48d76fbbbeb4c5b1d697bcab2caff11c42cb6a61
anzp <anzengping@momenta.ai>
2020-09-30 12:00:39 +0800

    add:added new params

--- Dockerfile
+++ Dockerfile
@@ -1,23 +1,27 @@
 ### stage: build ###
-FROM node:8.16.2-alpine AS build
+FROM artifactory.momenta.works/docker/node:lts-alpine AS builder
 
 WORKDIR /
+
 COPY package.json yarn.lock ./
-COPY / /
 
-RUN yarn install 
+RUN  yarn config set registry https://artifactory.momenta.works/artifactory/api/npm/npm/ \  
 
+  && yarn install 
 
-COPY tsconfig.json .
+COPY . /
 
 RUN yarn build
 
 ### stage: app ###
-FROM nginx:1.15-alpine AS app
+FROM nginx:alpine AS app
 
 LABEL maintainer="ZengPing An<anzengping@momenta.ai>"
+
 LABEL app=monaWebsite
 
 COPY nginx/ /etc/nginx/
+
+COPY  --from=builder dist/ /usr/share/nginx/html
 
 EXPOSE 80
--- src/components/tabcontent/tabcontent.tsx
+++ src/components/tabcontent/tabcontent.tsx
@@ -1,29 +1,28 @@
-import React, { useState, useEffect } from "react";
-import { Col, Button, Checkbox } from "antd";
-import { CheckboxChangeEvent } from "antd/lib/checkbox";
-import { Link } from "react-router-dom";
-import style from "./index.module.scss";
+import React, { useState, useEffect } from 'react'
+import { Col, Button, Checkbox } from 'antd'
+import { CheckboxChangeEvent } from 'antd/lib/checkbox'
+import { Link } from 'react-router-dom'
+import style from './index.module.scss'
 
 export const Tabcontent: React.FC<{
-  images: Object;
-  type: Object;
-  version: Object;
-}> = images => {
-  const [checked, setChecked] = useState<any>(false);
+  images: Object
+  type: Object
+  version: Object
+}> = (images) => {
+  const [checked, setChecked] = useState<any>(false)
 
   useEffect(() => {
-    let checkedInCookie = "1" === localStorage.getItem("checked");
-    setChecked(checkedInCookie);
-  }, []);
+    let checkedInCookie = '1' === localStorage.getItem('checked')
+    setChecked(checkedInCookie)
+  }, [])
 
   const onCheckboxChanged = (e: CheckboxChangeEvent) => {
-    setChecked(e.target.checked);
-    localStorage.setItem("checked", e.target.checked ? "1" : "0");
-  };
+    setChecked(e.target.checked)
+    localStorage.setItem('checked', e.target.checked ? '1' : '0')
+  }
 
-  const src = "/picInner/" + images["images"] + ".png";
-  const downloadSrc = `https://s3.cn-north-1.amazonaws.com.cn/momenta-download/mona-${images["version"]}.${images["type"]}`;
-
+  const src = '/picInner/' + images['images'] + '.png'
+  const downloadSrc = `https://s3.cn-north-1.amazonaws.com.cn/momenta-download/mona-${images['version'].toString().split('+').join('%2B')}.${images['type']}`
   return (
     <div className={style.tabContent}>
       <div className={style.topContent}>
@@ -32,33 +31,21 @@ export const Tabcontent: React.FC<{
             <img alt="roundtop" src={src} />
           </div>
           <span className={style.spanText}>
-            &nbsp;&nbsp;&nbsp;&nbsp;{images["images"]} ({images["version"]}.
-            {images["type"]})
+            &nbsp;&nbsp;{images['images']} ({images['version']}.{images['type']})
           </span>
         </Col>
         <div className={style.topButton}>
-          <Button
-            icon="download"
-            disabled={!checked}
-            type="primary"
-            size="large"
-            style={{ borderRadius: "20px", marginTop: "8px" }}
-            href={downloadSrc}
-          >
+          <Button icon="download" disabled={!checked} type="primary" size="large" style={{ borderRadius: '20px', marginTop: '8px' }} href={downloadSrc}>
             下载
           </Button>
         </div>
       </div>
       <div className={style.bottomContent}>
-        <Checkbox
-          autoFocus={false}
-          onChange={onCheckboxChanged}
-          checked={checked}
-        >
+        <Checkbox autoFocus={false} onChange={onCheckboxChanged} checked={checked}>
           同意软件
           <Link to="/terms">使用协议</Link>
         </Checkbox>
       </div>
     </div>
-  );
-};
+  )
+}
